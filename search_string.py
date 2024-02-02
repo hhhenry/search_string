04cf6c2df85806319fb9b2f81955ec1205a55727
hhhenry <lihaoranhiei@163.com>
2024-02-02 15:55:40 +0800

    Create search_string.py

---
+++ search_string.py
@@ -0,0 +1,15 @@
+import os
+
+def search_string(path, str):
+    f = os.walk(path)
+    for root, dirs, files in f:
+        for file in files:
+            j = open(path + file, 'r', encoding='utf-8-sig')
+            file_contents = j.readlines()
+            for content in file_contents:
+                if str in content:
+                    print(file)
+                    
+path = r"C:\\code"
+str = 'str'
+search_string(path, str)
